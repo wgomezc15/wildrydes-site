e244216d2b639cfe5b6ecdb7525ed4fe0f339cbb
William Gomez <wgomezc15@gmail.com>
2023-03-14 20:38:25 -0500

    new_config 2

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_6A7niPLmt', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3n2817pg8fuil302i962h76ara', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '3n2817pg8fuil302i962h76ara' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
